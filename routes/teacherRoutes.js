3ac435cc3d37f54de84a3d1e067f8b32a89d5214
Gracy Bagga <bagga2131@saskpolytech.ca>
2025-03-30 20:35:23 -0600

    Added RBAC in teacher end points

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -6,7 +6,7 @@ const { getTeacherDashboard, getTeacherProfile } = require("../controllers/teach
 const router = express.Router();
 
 // Ensure that only teachers can access the teacher dashboard
-router.get("/dashboard/teacher/:userId", verifyToken, verifyRole("TEACHER"), getTeacherDashboard);// GB 032925
+router.get("/dashboard/:userId", verifyToken, verifyRole("TEACHER"), getTeacherDashboard);// GB 032925
 
 // Get teacher profile
 router.get("/profile/:userId", verifyToken, verifyRole("TEACHER"), getTeacherProfile);// GB 032925
